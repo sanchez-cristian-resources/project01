66fa2d83ef016f7b293a984e6cffd5da42897bf9
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-11-20 20:27:33 +0100

    👷 Deploy Netlify: Deploy for testing router performance

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 import { routes } from './data/routes.js'
 
-async function loadPage(path) {
+function getPath() {
+    const url = window.location.pathname
+    let path = url.split('/').splice(1) 
+
+    if(String(path) === '') path = 'home'
+    else path = String(path)
+
+    return path
+}
+
+async function loadPage() {
+    const path = getPath()
+
     const view = document.querySelector('#view')
     const page = await fetchPage(path)
 
@@ -22,19 +34,19 @@ async function fetchPage(path) {
         const header = document.querySelector('header')
         header.style.display = 'none'
     }
+    
+    // Set all meta-elements: css, searchbar, title & description
+    configurePage(route, path)
  
     // fetch the route
     const page = await fetch(route.url)
         .then((page) => page.text()) // .text() returns a promise
 
-    
-    // Set all meta-elements: css, searchbar, title & description
-    configurePage(route)
 
     return page
 }
 
-function configurePage(route) {
+function configurePage(route, path) {
     // change style
     const style = document.querySelector('#style')
     style.setAttribute('href', route.style)
@@ -46,9 +58,45 @@ function configurePage(route) {
     // change title
     const description = document.querySelector('#description')
     description.setAttribute('content', route.description)
+
+    // change highlighted li -> empty string to enforce to use css
+    if( path === 'home' ){
+        document.querySelector('#home').style.color = '#072ac8'
+        document.querySelector('#projects').style.color = ''
+        document.querySelector('#services').style.color = ''
+    } else if( path === 'projects' ){
+        document.querySelector('#home').style.color = ''
+        document.querySelector('#projects').style.color = '#072ac8'
+        document.querySelector('#services').style.color = ''
+    } else if( path === 'services' ){
+        document.querySelector('#home').style.color = ''
+        document.querySelector('#projects').style.color = ''
+        document.querySelector('#services').style.color = '#072ac8'
+    }
 }
 
 function addNavbarListeners() {
+    // DESKTOP
+    const home = document.querySelector('#home')
+    const projects = document.querySelector('#projects')
+    const services = document.querySelector('#services')
+
+    home.addEventListener('click', () => {
+        window.history.pushState({}, "", './');
+        loadPage() 
+    })
+
+    projects.addEventListener('click', () => {
+        window.history.pushState({}, "", 'projects');
+        loadPage() 
+    })
+
+    services.addEventListener('click', () => {
+        window.history.pushState({}, "", 'services');
+        loadPage() 
+    })
+
+    // MOBILE
     const button = document.querySelector('#menu-button')
     const menu = document.querySelector('#menu')
 
@@ -69,14 +117,8 @@ function addNavbarListeners() {
 }
 
 window.addEventListener('load', () => {
-    const url = window.location.pathname
-    let path = url.split('/').splice(1) 
-
-    if(String(path) === '') path = 'home'
-    else path = String(path)
-
-    loadPage(path)
-
+    // LOAD DEFAULT PAGE ( HOMEPAGE )
+    loadPage()
 
     // NAVBAR BUTTON (MOBILE) 
     addNavbarListeners()
