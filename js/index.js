d816772eb7a15b2eb3a3d1ac3464f03138093fcf
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-11-21 18:00:31 +0100

    ♻️  Router class: Now the routing is managed by Router class

--- js/index.js
+++ js/index.js
@@ -1,66 +1,10 @@
-import { routes } from './data/routes.js'
+import Router from './router.js'
 
-function getPath () {
-  const url = window.location.pathname // + 'contact'
-  let path = url.split('/').splice(1)
-
-  if (String(path) === '') path = 'home'
-  else path = String(path)
-
-  return path
-}
-
-async function loadPage () {
-  const path = getPath()
-
-  const view = document.querySelector('#view')
-  const page = await fetchPage(path)
-  view.innerHTML = page
-}
-
-async function fetchPage (path) {
-  const validRoutes = Object.keys(routes)
-
-  // get the current route
-  let route
-
-  for (const validRoute of validRoutes) {
-    if (validRoute === path) route = routes[path]
-  }
-
-  if (!route) {
-    route = routes['404']
-    const header = document.querySelector('header')
-    header.style.display = 'none'
-  }
-
-  // Set all meta-elements: css, searchbar, title & description
-  configurePage(route, path)
-
-  // fetch the route
-  const page = await fetch(route.url)
-    .then((page) => page.text()) // .text() returns a promise
-
-  return page
-}
-
-function configurePage (route, path) {
-  // change style
-  const style = document.querySelector('#style')
-  style.setAttribute('href', route.style)
-
-  // change title
-  const title = document.querySelector('title')
-  title.innerHTML = route.title
-
-  // change title
-  const description = document.querySelector('#description')
-  description.setAttribute('content', route.description)
-}
+const router = new Router()
 
 window.addEventListener('load', () => {
   // LOAD DEFAULT PAGE ( HOMEPAGE )
-  loadPage()
+  router.loadPage()
 
   // NAVBAR BUTTON (MOBILE)
   addNavbarListeners()
@@ -75,17 +19,17 @@ function addNavbarListeners () {
 
   contact.addEventListener('click', () => {
     window.history.pushState({}, '', './contact')
-    loadPage()
+    router.loadPage()
   })
 
   home.addEventListener('click', () => {
     window.history.pushState({}, '', './')
-    loadPage()
+    router.loadPage()
   })
 
   projects.addEventListener('click', () => {
-    window.history.pushState({}, '', './#third-section')
-    loadPage()
+    window.history.pushState({}, '', './')
+    router.loadPage()
 
     const position = document.querySelector('#third-section').offsetTop
     window.scrollTo(0, (position - 150))
@@ -93,7 +37,7 @@ function addNavbarListeners () {
 
   services.addEventListener('click', () => {
     window.history.pushState({}, '', './')
-    loadPage()
+    router.loadPage()
 
     const position = document.querySelector('#fifth-section').offsetTop
     window.scrollTo(0, (position - 150))
