66e1c0c343cd61605e9c340234adc511f2c401db
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-11-23 17:24:49 +0100

    ✨ _project Page: Now is accessible the project page on card click

--- js/Pages/home.js
+++ js/Pages/home.js
@@ -1,40 +1,18 @@
-import { createContext } from '../Context/Context.js'
 import Router from '../Router/router.js'
+import { fetchProjects } from '../APIs/fetchProjects.js'
 
-const [projects, setProjects] = createContext([])
-
-export async function fetchProjects () {
+export async function getProjects () {
   const router = new Router()
-  await fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
-    .then(data => data.json())
-    .then(data => {
-      setProjects(data)
-      console.log('he hecho fethc de ', data)
-    })
-
-  // add a comparable element like time => in milliseconds
-  const datedProjects = [...projects()].map((project) => {
-    const date = new Date(project.completed_on)
-    const id = project.name
-      .replace(' ', '-')
-      .toLowerCase()
-    return { ...project, milliseconds: date.getTime(), id }
-  })
-
-  // order the projects by the milliseconds
-  const oProjects = datedProjects.sort((a, b) => b.milliseconds - a.milliseconds)
-
-  setProjects(oProjects)
-  console.log(projects())
+  const projects = await fetchProjects()
 
   let cards = ''
   for (let i = 0; i < 3; i++) {
     cards += `
-      <div id="${oProjects[i].id}" class="card">
-        <img loading="eager" src="${oProjects[i].image}" alt="Simplify project image">
+      <div id="${projects[i].id}" class="card">
+        <img loading="eager" src="${projects[i].image}" alt="Simplify project image">
         <div class="text-container">
-          <p class="title">${oProjects[i].name}</p>
-          <p class="description">${oProjects[i].description}</p>
+          <p class="title">${projects[i].name}</p>
+          <p class="description">${projects[i].description}</p>
           <p class="link">Learn more</p>
         </div>
       </div>
@@ -43,8 +21,8 @@ export async function fetchProjects () {
 
   document.querySelector('#cards-container').innerHTML = cards
   for (let i = 0; i < 3; i++) {
-    document.querySelector(`#${oProjects[i].id}`).addEventListener('click', (e) => {
-      window.history.pushState({}, '', `/projects/${oProjects[i].id}`)
+    document.querySelector(`#${projects[i].id}`).addEventListener('click', (e) => {
+      window.history.pushState({}, '', `/projects/${projects[i].id}`)
       router.loadPage()
     })
   }
