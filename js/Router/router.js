8a1a007c6353c07946f5c8adcef4b5a889830304
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-11-23 12:43:57 +0100

    ✨Nested routes: now you can declare nested routes at one level </example/id>

--- js/Router/router.js
+++ js/Router/router.js
@@ -3,6 +3,7 @@ import { routes } from './routes.js'
 class Router {
   constructor () {
     this.path = ''
+    this.segments = []
     this.route = ''
   }
 
@@ -13,8 +14,14 @@ class Router {
     else this.path = '/'
   }
 
+  getSegments () {
+    const segments = this.path.split('/').splice(1) 
+    this.segments = segments 
+  }
+
   async loadPage () {
     this.getCurrentPath()
+    this.getSegments()
 
     const view = document.querySelector('#view')
     const page = await this.fetchPage()
@@ -30,8 +37,9 @@ class Router {
       if (validRoute === this.path) this.route = routes[this.path]
     }
 
-    if (!this.route) {
-      this.route = routes['404']
+    //check if the route has childs before throwing 404 page
+    if (!this.route && !this.hasChild()) {
+      this.route = routes['/404']
       const header = document.querySelector('header')
       header.style.display = 'none'
     }
@@ -62,6 +70,27 @@ class Router {
 
   execOptions () {
     this.route.options.map((option) => option())
+  }
+
+  hasChild() {
+    // if the first segment route exists continue
+    if( !routes[`/${this.segments[0]}`] ) return 
+    if( this.segments.length <= 1 ) return 
+    
+    console.log(routes[`/${this.segments[0]}`],this.segments[0] )
+    const parentRoute = routes[`/${this.segments[0]}`]
+    const requested = this.segments[1]
+
+    for( let i = 0; i < parentRoute.childs.length; i++){
+      console.log(requested[i], parentRoute.childs[i][requested])
+      if(parentRoute.childs[i][requested]) {
+        //Access the property to return the property object
+        this.route = parentRoute.childs[i][requested]
+        return true
+      }
+    }
+
+    return false
   }
 
   // GETTERS AND SETTERS
